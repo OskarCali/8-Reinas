6402345baaa2aee6d0ff150a46738b9bc0bebab5
Oskar <oskarcali18@gmail.com>
2019-07-04 04:28:49 -0500

    Add button Click Function
    Creacion del dato a traves del controlador y asignacion a la nueva forma

--- 8 Reinas/8 Reinas/View/Menu.cs
+++ 8 Reinas/8 Reinas/View/Menu.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using _8_Reinas.Controller;
+using _8_Reinas.View;
 
 namespace _8_Reinas
 {
@@ -15,6 +17,18 @@ namespace _8_Reinas
         public formMenu()
         {
             InitializeComponent();
+        }
+
+        private void BtnEnter_Click(object sender, EventArgs e)
+        {
+            ControllerFunction controller = new ControllerFunction();
+
+            formHome home = new formHome();
+            home.Dato = controller.datos(Convert.ToInt32(poblacionNumUD.ToString()), Convert.ToInt32(iteracionesNumUD.ToString()), Convert.ToDecimal(probCruceNumUD.ToString()), Convert.ToDecimal(probMutacionNumUD.ToString()));
+
+            Hide();
+            home.ShowDialog(this);
+            Show();
         }
     }
 }
