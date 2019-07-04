88860a19a03867107d8e95919f55952ae2734eed
Oskar <oskarcali18@gmail.com>
2019-07-04 03:09:36 -0500

    Add Menu View
    Vista menu para seleccionar datos iniciales

--- 8 Reinas/8 Reinas/View/Menu.Designer.cs
+++ 8 Reinas/8 Reinas/View/Menu.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace _8_Reinas
 {
-    partial class Form1
+    partial class Menu
     {
         /// <summary>
         /// Required designer variable.
@@ -31,7 +31,7 @@
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Text = "Menu";
         }
 
         #endregion
